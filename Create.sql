dbd95c77bad5419c38b7c808cb2e673041295c2a
LI, Wai Yin <p768lwy3@gmail.com>
2018-05-09 14:24:10 +0800

    Add files via upload

---
+++ Create.sql
@@ -0,0 +1,74 @@
+CREATE TABLE HorseInfo (
+    HorseID varchar(4) NOT NULL, 
+    Name varchar(64), 
+    Country varchar(3), 
+    Age int, 
+    Color varchar(16),
+    Sex varchar(16), 
+    ImportType varchar(3), 
+    SeasonStakes int, 
+    TotalStakes int, 
+    123Starts varchar(16),
+    StartsPast10 int, 
+    Trainer varchar(64), 
+    Owner varchar(64), 
+    CurrentRating int, 
+    StartRating int,
+    Sire varchar(64), 
+    Dam varchar(64), 
+    DamSire varchar(64), 
+    PRIMARY KEY(HorseNo)
+);
+
+CREATE TABLE PastRecord (
+    RaceID varchar(10) NOT NULL,
+    HorseID varchar(4) NOT NULL,
+    RaceIndex int,
+    Pla int,
+    RaceDate DATE,
+    RC varchar(2),
+    Track varchar(8),
+    Course varchar(8),
+    Dist int,
+    G varchar(8),
+    RaceClass varchar(8),
+    Dr int,
+    Rtg int,
+    Trainer varchar(64),
+    Jockey varchar(64),
+    LBW varchar(12),
+    WinOdds float,
+    ActWt int,
+    RunningPosition varchar(32),
+    FinishTime varchar(7),
+    DeclarHorseWt int,
+    Gear varchar(16),
+    PRIMARY KEY(RaceNo, HorseNo)
+);
+
+CREATE TABLE GameInfo (
+    RaceID varchar(10) NOT NULL,
+    RaceClass varchar(8),
+    Dist int,
+    Going varchar(16), 
+    CourseType varchar(16),
+    Course varchar(16), 
+    Bouns int,
+    PRIMARY KEY(GameID)
+);
+
+CREATE TABLE GameRecord (
+    RaceID varchar(10) NOT NULL,
+    HorseID varchar(4) NOT NULL,
+    Pla int,
+    HorseNo int, 
+    Jockey varchar(64),
+    Trainer varchar(64),
+    ActWt int,
+    DeclarHorseWt int,
+    Dr int,
+    LBW varchar(12),
+    FinishTime varchar(7),
+    WinOdds float,
+    PRIMARY KEY(GameID, HorseID)
+);
---
+++ main.py
@@ -0,0 +1,11 @@
+import dbm
+
+if __name__ == "__main__":
+    db = dbm.database()
+    db.gameupdate()
+    #db.__updategame__("20120512")
+    #db.__updategame__("20080507")
+    #db.__crawlgame__("http://racing.hkjc.com/racing/info/meeting/Results/English/Local/20120512/ST/2")
+    #db.horseupdate()
+    #db.__updatehorseinfo__('T193')
+    #db.__updatepastrecord__('T193')
